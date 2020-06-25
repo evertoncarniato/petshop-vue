9fea9ea202e6a5b8d9bfefeb3c31b2b0757757ed
Everton Carniato Firmino de Oliveira <everton.carniato@gmail.com>
2020-06-25 17:37:51 -0300

    consumindo api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <td>Nome</td>
+          <td>Descrição</td>
+          <td>Valor</td>
+          <td>Desconto</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="produto in produtos" :key="produto._id">
+          <td>{{ produto.nome }}</td>
+          <td>{{ produto.descricao }}</td>
+          <td>R${{ produto.valor }}</td>
+          <td>{{ produto.desconto }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      produtos: null
+    }
+  },
+  mounted(){
+    this.getProdutos();
+  },
+  methods: {
+    getProdutos(){
+      axios
+        .get('https://server-node-example.herokuapp.com/produtos')
+          .then( response => {
+            console.log(response)
+            this.produtos = response.data
+          })
+          .catch( e=> console.log(e) )
+    }
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+thead tr {
+  font-weight: bold;
 }
 </style>
