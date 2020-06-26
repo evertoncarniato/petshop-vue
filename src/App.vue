4954ce19f5e110107690ed601f2cd76c45427873
Everton Carniato Firmino de Oliveira <everton.carniato@gmail.com>
2020-06-25 22:30:39 -0300

    Colocando dados em cards

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,51 @@
 <template>
   <div>
-    <table>
-      <thead>
-        <tr>
-          <td>Nome</td>
-          <td>Descrição</td>
-          <td>Valor</td>
-          <td>Desconto</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="produto in produtos" :key="produto._id">
-          <td>{{ produto.nome }}</td>
-          <td>{{ produto.descricao }}</td>
-          <td>R${{ produto.valor }}</td>
-          <td>{{ produto.desconto }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <b-card-grouo columns>
+      <b-card v-for="produto in produtos" v-bind:key="produto._id" v-bind:title="produto.nome">
+        <b-card-text>{{ produto.descricao }}</b-card-text>
+        <div v-if="produto.desconto == ''">
+          <b-card-text>R${{ produto.valor }}</b-card-text>
+        </div>
+        <div v-else>
+          <b-card-text>De: <span id="valorAntigo">R${{ produto.valor }}</span></b-card-text>
+          <b-card-text>Por: R${{ produto.valor }}</b-card-text>
+        </div>
+      </b-card>
+    </b-card-grouo>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  data(){
+  data() {
     return {
       produtos: null
-    }
+    };
   },
-  mounted(){
+  mounted() {
     this.getProdutos();
   },
   methods: {
-    getProdutos(){
+    getProdutos() {
       axios
-        .get('https://server-node-example.herokuapp.com/produtos')
-          .then( response => {
-            console.log(response)
-            this.produtos = response.data
-          })
-          .catch( e=> console.log(e) )
+        .get("https://server-node-example.herokuapp.com/produtos")
+        .then(response => {
+          console.log(response.data);
+          this.produtos = response.data;
+        })
+        .catch(e => console.log(e));
     }
   }
-}
-
+};
 </script>
 
 <style>
 thead tr {
   font-weight: bold;
+}
+#valorAntigo {
+  text-decoration: line-through;
 }
 </style>
